585af847f084def593c59c619945c8c451e4787f
M Braga <mateus.braga@rebase.com.br>
2020-01-08 10:55:58 -0300

    added comments

--- lib/code_signal/calc_area.ex
+++ lib/code_signal/calc_area.ex
@@ -1,5 +1,10 @@
 defmodule Calculate do
   
+  # The answer delivered was 
+  # def shape_area(n) when 104 > n and n >= 1  do
+  #   n * n + (n - 1) * (n - 1)
+  # end 
+  
   def shape_area(n) when 104 > n and n >= 1  do
     current = multiply(n)
     lesser = multiply(n - 1)
